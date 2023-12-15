ab170de9af2724bc127c2254d9b74bd85bd8f271
KyoWang <wangkegang@kuaishou.com>
2023-12-15 18:30:45 +0800

    done

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="123" &&  pwd=="123"){
+    if(userName=="小金戈" &&  pwd=="19941217"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
 			setTimeout(function(){location.href="BirthdayCake.html";},2000);
 		}
 	else{
-		alert("Wrong Password");
+		alert("怎么出错了呢？🤔");
 	}
 });
